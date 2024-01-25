0ca54878ba4b980659081b56fb77ccb327e02ae1
Fernando Harris <fernando.harris@gmail.com>
2024-01-25 15:39:26 +0100

    bean

--- src/main/java/oraclecloudnative/ocilab/curiosity/curiosity/serviceclients/ChampionshipServiceEventPublisher.java
+++ src/main/java/oraclecloudnative/ocilab/curiosity/curiosity/serviceclients/ChampionshipServiceEventPublisher.java
@@ -1,7 +1,16 @@
 package oraclecloudnative.ocilab.curiosity.curiosity.serviceclients;
 
 import oraclecloudnative.ocilab.curiosity.curiosity.QueryPage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+/*
+*
+* Publish QueryPage objects to Kafka Broker 
+*
+*/
+@Slf4j
+@Service
 public class ChampionshipServiceEventPublisher {
 
     public void publishMessageToKafka(QueryPage queryPage) {
