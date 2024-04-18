b06671e4e8a7df5e4d99b27eb3a4e124275a8a66
Fernando Harris <fernando.harris@oracle.com>
2024-04-18 11:39:55 +0200

    giving all privileged in championshipdb to curiosity user

--- databaseconfig/create-curiositydb-resources.sql
+++ databaseconfig/create-curiositydb-resources.sql
@@ -8,5 +8,6 @@ CREATE USER 'curiosity'@'%' IDENTIFIED BY 'Welcome#1';
 CREATE USER 'championship'@'%' IDENTIFIED BY 'Welcome#1';
 GRANT ALL PRIVILEGES ON curiositydb.* TO 'curiosity'@'%'; 
 GRANT ALL PRIVILEGES ON championshipdb.* TO 'championship'@'%'; 
+GRANT ALL PRIVILEGES ON championshipdb.* TO 'curiosity'@'%'; 
 SHOW DATABASES;
 
